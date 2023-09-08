7f910dedfbaa74d15fb7155fe95d5cd222d027db
Robb Charnock <robbchar@gmail.com>
2023-09-07 20:48:14 -0700

    adding the question component

--- components/Answer/index.tsx
+++ components/Answer/index.tsx
@@ -3,7 +3,7 @@ import styles from './Answer.module.css';
 export default function Home() {
   return (
     <div className={styles.answer}>
-      <p>This is an ansswer that I have made up.</p>
+      <p>This is an answer that I have made up.</p>
     </div>
   );
 }
---
+++ components/Question/index.tsx
@@ -0,0 +1,9 @@
+import styles from './Question.module.css';
+
+export default function Question() {
+  return (
+    <div className={styles.question}>
+      <h1>This is a question I have made up.</h1>
+    </div>
+  );
+}
