646ab23fb36a8a39cf92e96c6284ae2367b4945c
Robb Charnock <rhcharnock@bamboohealth.com>
2023-09-20 13:23:35 -0700

    working teams page

--- components/TeamLayout/index.tsx
+++ components/TeamLayout/index.tsx
@@ -4,12 +4,72 @@ import TeamName from '../TeamName';
 import { useRouter } from 'next/router';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTableCells } from '@fortawesome/free-solid-svg-icons';
+import { useState, useEffect } from 'react';
 
 export default function TeamLayout() {
+  const [teamsData, setTeamsData] = useState({ teams: [] });
   const router = useRouter();
+
+  useEffect(() => {
+    setTeamsData(
+      window.localStorage.getItem('teamsData')
+        ? JSON.parse(window.localStorage.getItem('teamsData'))
+        : { teams: [] },
+    );
+  }, []);
 
   const goQuestionsHome = () => {
     router.push('/questions');
+  };
+
+  const sortTeams = () => {
+    teamsData.teams = teamsData.teams.sort((a, b) =>
+      a.points > b.points ? -1 : 1,
+    );
+    setTeamsData(JSON.parse(JSON.stringify(teamsData)));
+    saveData();
+  };
+
+  const addTeam = () => {
+    teamsData.teams.push({ name: '', points: 0 });
+    setTeamsData(JSON.parse(JSON.stringify(teamsData)));
+  };
+
+  const updateTeam = (index, name, points) => {
+    if (teamsData.teams.length === 0) {
+      teamsData.teams.push({ name, points });
+    } else {
+      teamsData.teams[index].name = name;
+      teamsData.teams[index].points = points;
+    }
+    saveData();
+    setTeamsData(JSON.parse(JSON.stringify(teamsData)));
+    renderTeams();
+  };
+
+  const saveData = () => {
+    window.localStorage.setItem('teamsData', JSON.stringify(teamsData));
+  };
+
+  const renderTeams = () => {
+    return teamsData.teams.length === 0 ? (
+      <TeamName
+        name={'Temp Name'}
+        points="0"
+        index="0"
+        updateTeam={updateTeam}
+      />
+    ) : (
+      teamsData.teams.map((team, index) => (
+        <TeamName
+          name={team.name}
+          points={team.points}
+          index={index}
+          updateTeam={updateTeam}
+          key={index}
+        />
+      ))
+    );
   };
 
   return (
@@ -21,11 +81,15 @@ export default function TeamLayout() {
       </NavHeader>
       <div className={styles.content}>
         <div className={styles.teamsContainer}>
-          <div className={styles.namesContainer}>
-            <TeamName />
-            <TeamName />
+          <div className={styles.namesContainer}>{renderTeams()}</div>
+          <div className={styles.buttonsContainer}>
+            <div className={styles.button} onClick={sortTeams}>
+              Sort
+            </div>
+            <div className={styles.button} onClick={addTeam}>
+              Add Team
+            </div>
           </div>
-          <div className={styles.buttonsContainer}>button 1</div>
         </div>
       </div>
     </>
--- components/TeamName/index.tsx
+++ components/TeamName/index.tsx
@@ -4,21 +4,65 @@ import {
   faSquareCaretUp,
   faSquareCaretDown,
 } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
 
-export default function TeamName() {
+export default function TeamName({ name, points, index, updateTeam }) {
+  const [isEditing, setIsEditing] = useState(!name);
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      setIsEditing(false);
+      updateTeam(index, event.target.value, 0);
+    }
+  };
+
+  const handleTeamClick = (e) => {
+    switch (e.detail) {
+      case 2:
+        setIsEditing(true);
+        break;
+    }
+  };
+
+  const handlePointsClick = (upOrDown) => {
+    if (upOrDown === 'up') {
+      updateTeam(index, name, Number.parseInt(points) + 1);
+    } else if (upOrDown === 'down' && Number.parseInt(points) > 0) {
+      updateTeam(index, name, Number.parseInt(points) - 1);
+    }
+  };
+
   return (
     <div className={styles.content}>
-      <input className={styles.nameInput} type="text" placeholder="Team Name" />
+      {isEditing ? (
+        <input
+          className={styles.teamName}
+          type="text"
+          placeholder="Team Name"
+          defaultValue={name}
+          onKeyDown={handleKeyDown}
+        />
+      ) : (
+        <h2 className={styles.teamName} onClick={handleTeamClick}>
+          {name}
+        </h2>
+      )}
       <div className={styles.numberContainer}>
         <div className={styles.number}>
-          <span>0</span>
+          <span>{points}</span>
         </div>
         <div className={styles.buttonContainer}>
-          <div className={styles.increment}>
+          <div
+            className={styles.increment}
+            onClick={() => handlePointsClick('up')}
+          >
             <FontAwesomeIcon icon={faSquareCaretUp} />
           </div>
           <div className={styles.decrement}>
-            <FontAwesomeIcon icon={faSquareCaretDown} />
+            <FontAwesomeIcon
+              icon={faSquareCaretDown}
+              onClick={() => handlePointsClick('down')}
+            />
           </div>
         </div>
       </div>
