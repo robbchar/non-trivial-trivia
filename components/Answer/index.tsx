5f7b89df948da0d1ff698096f6f1a0eefbf438d7
Robb Charnock <rhcharnock@bamboohealth.com>
2023-09-20 15:33:27 -0700

    bug in the way answers were revealed

--- components/Answer/index.tsx
+++ components/Answer/index.tsx
@@ -1,20 +1,18 @@
 import styles from './Answer.module.css';
-import { useContext, useEffect, useState } from 'react';
-import { TriviaContext } from '../../components/TriviaContext';
+import { useEffect, useState } from 'react';
 
 export default function Answer({ answer, revealAnswer }) {
   const [className, setClassName] = useState(styles.answer);
-  const context = useContext(TriviaContext);
-
+  console.log('answer', revealAnswer);
   useEffect(() => {
-    if (context.revealAnswer) {
+    if (revealAnswer) {
       setClassName(answer.correct ? styles.correctAnswer : styles.wrongAnswer);
     } else {
       setClassName(styles.answer);
     }
   }, [revealAnswer]);
 
-  console.log({ revealAnswer });
+  console.log({ revealAnswer, className });
   return (
     <div className={className}>
       <p>{answer.text}</p>
